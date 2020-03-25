df4ed845a985fe6617c2d87ac6889e148ae14922
GekaRudenko <heltyrjvbi@gmail.com>
2020-03-25 10:15:57 +0200

    add mvc and main controler, first page - gritting.html

--- src/main/java/com/rudenko/knowledgeTesting/controllers/MainController.java
+++ src/main/java/com/rudenko/knowledgeTesting/controllers/MainController.java
@@ -1,4 +1,21 @@
 package com.rudenko.knowledgeTesting.controllers;
 
+
+import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MainController {
+
+
+    @GetMapping("/greeting")//обрабатывет юрл адрес /greeting типа слушатель и когда видит выполняется метод greeting
+    //если просто / то ето главная
+    public String greeting( Model model) {//метод greeting
+        model.addAttribute("name", "ANNA");//параметр model  по названию name = значение "ANNA"
+        // будет передан в шаблон return "greeting";
+        return "greeting";  //вызываем шалон под названием greeting
+    }
+
 }
